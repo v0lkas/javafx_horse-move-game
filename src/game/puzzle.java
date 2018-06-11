030c15197b8d4f2838a4247099d5f49e8d3a5831
Vitalij V <38693036+v0lkas@users.noreply.github.com>
2018-06-11 07:39:45 +0300

    Start

---
+++ src/game/puzzle.java
@@ -0,0 +1,10 @@
+//import game.gui.Main_window;
+//
+//package game;
+//
+//public class puzzle {
+//    public static void main(String[] args){
+//        MyFrame f = new MyFrame();
+//        f.setVisible(true);
+//    }
+//}
